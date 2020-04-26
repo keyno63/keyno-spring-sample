4ca27cbffc6dc272d7a9d80cd41eee2bba8a7d15
Keyno <kofujiw@gmail.com>
2020-04-27 06:45:53 +0900

    add spring initializer files

---
+++ src/main/java/com/keyno/keynospringsample/KeynoSpringSampleApplication.java
@@ -0,0 +1,13 @@
+package com.keyno.keynospringsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeynoSpringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeynoSpringSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keyno/keynospringsample/KeynoSpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyno.keynospringsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeynoSpringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
