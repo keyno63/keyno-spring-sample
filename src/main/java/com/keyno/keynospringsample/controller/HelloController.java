702607af24232e6739286c180ea155451ea9d156
Keyno <kofujiw@gmail.com>
2020-04-29 00:24:00 +0900

    Request param 使ってみる

--- src/main/java/com/keyno/keynospringsample/controller/HelloController.java
+++ src/main/java/com/keyno/keynospringsample/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.keyno.keynospringsample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.keyno.keynospringsample.service.HelloService;
 
@@ -18,14 +19,20 @@ public class HelloController {
     }
 
     @GetMapping("/get")
-    public String get()
+    public String get(
+            @RequestParam String type_a,
+            @RequestParam(required = false) String type_b
+    )
     {
-        return helloService.get();
+        return helloService.get(type_a, type_b);
     }
 
     @GetMapping("/map")
-    public Map<String, String> map()
+    public Map<String, String> map(
+            @RequestParam(name = "key") String key,
+            @RequestParam(name = "value") String value
+    )
     {
-        return Map.of("key", "value");
+        return Map.of(key, value);
     }
 }
--- src/main/java/com/keyno/keynospringsample/service/HelloService.java
+++ src/main/java/com/keyno/keynospringsample/service/HelloService.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
 
-    public String get()
+    public String get(String type_a, String type_b)
     {
-        return "";
+        StringBuilder sb = new StringBuilder()
+                .append("request:")
+                .append(type_a)
+                .append(",")
+                .append(type_b);
+        return sb.toString();
     }
 }
