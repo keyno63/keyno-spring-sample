a1ca5372c6e8ac008d1ae5e11f466e990eae9ad4
Keyno <kofujiw@gmail.com>
2020-04-29 00:11:14 +0900

    Map の Json 返すサンプル

--- src/main/java/com/keyno/keynospringsample/controller/HelloController.java
+++ src/main/java/com/keyno/keynospringsample/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
@@ -12,5 +14,11 @@ public class HelloController {
     public String get()
     {
         return "Hello World";
+    }
+
+    @GetMapping("/map")
+    public Map<String, String> map()
+    {
+        return Map.of("key", "value");
     }
 }
